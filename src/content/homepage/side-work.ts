20ce464c2aa74aba8929b8db76ac659ad58329b4
Iggy Kuperson <i.kupchinenko@gmail.com>
2026-08-21 08:34:14 +0300

    rm docs

---
+++ src/content/homepage/side-work.ts
@@ -0,0 +1,8 @@
+export const selectedXPosts = [
+  {
+    id: 'switch-tension',
+    label: 'A note on interaction details',
+    text: 'I added more character to the switch by making the thumb build up tension before it moves. It squashes, accelerates non-linearly, then compresses the opposite side on impact. I also made the color transition follow the thumb’s movement. The difference is in the details. They’re what make something ordinary feel special.',
+    href: 'https://x.com/kidpudel/status/2089804412319358998',
+  },
+] as const;
